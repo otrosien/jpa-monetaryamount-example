e2b4d1295baabc1a431bfe2056a598e4bee8b53b
Oliver Trosien <otrosien@epages.com>
2016-10-13 13:28:12 +0200

    use attributeaccessor

--- src/main/java/com/example/WriteCurrencyTrans.java
+++ src/main/java/com/example/WriteCurrencyTrans.java
@@ -1,16 +1,26 @@
 package com.example;
 
+import java.math.BigDecimal;
+
 import javax.money.MonetaryAmount;
 
+import org.eclipse.persistence.mappings.AttributeAccessor;
+import org.eclipse.persistence.mappings.foundation.AbstractTransformationMapping;
 import org.eclipse.persistence.mappings.transformers.FieldTransformerAdapter;
 import org.eclipse.persistence.sessions.Session;
 
 @SuppressWarnings("serial")
 public class WriteCurrencyTrans extends FieldTransformerAdapter {
+
+    private AttributeAccessor attributeAccessor;
+
     @Override
+    public void initialize(AbstractTransformationMapping mapping) {
+        this.attributeAccessor = mapping.getAttributeAccessor();
+    }
+
     public String buildFieldValue(Object object, String fieldName, Session session) {
-        Product product = (Product) object;
-        MonetaryAmount amount = product.getPrice();
+        MonetaryAmount amount = (MonetaryAmount) attributeAccessor.getAttributeValueFromObject(object);
         if(amount == null) {
             return null;
         }
