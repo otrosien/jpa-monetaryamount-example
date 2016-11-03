0b80b735a7078e167243494b89520f794776d7d7
Oliver Trosien <otrosien@epages.com>
2016-11-03 10:37:08 +0100

    imports cleanup

--- src/main/java/com/example/CurrencyUnitDeserializer.java
+++ src/main/java/com/example/CurrencyUnitDeserializer.java
@@ -4,12 +4,8 @@ import java.io.IOException;
 
 import org.joda.money.CurrencyUnit;
 
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.deser.ValueInstantiator;
-import com.fasterxml.jackson.databind.deser.ValueInstantiators;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 
 public class CurrencyUnitDeserializer extends ValueInstantiator {
 
@@ -17,12 +13,6 @@ public class CurrencyUnitDeserializer extends ValueInstantiator {
     public boolean canCreateFromString() {
         return true;
     }
-//
-//    @Override
-//    public CurrencyUnit deserialize(JsonParser parser, DeserializationContext ctxt) throws IOException {
-//        final String currencyCode = parser.getValueAsString();
-//        return CurrencyUnit.of(currencyCode);
-//    }
 
     @Override
     public Object createFromString(DeserializationContext ctxt, String value) throws IOException {
--- src/main/java/com/example/CurrencyUnitSerializer.java
+++ src/main/java/com/example/CurrencyUnitSerializer.java
@@ -2,15 +2,11 @@ package com.example;
 
 import java.io.IOException;
 
-import org.joda.money.CurrencyUnit;
-
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
+@SuppressWarnings("serial")
 public class CurrencyUnitSerializer extends ToStringSerializer {
 
     @Override
